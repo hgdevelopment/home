ca5838661666d6eceb804d6a129c5314c7250a8c
Administrator@HEERA.COM <Administrator@HEERA.COM>
2017-11-07 10:47:17 +0530

    minor update for testing

--- app/Http/Controllers/web/memberController.php
+++ app/Http/Controllers/web/memberController.php
@@ -13,7 +13,6 @@ use App\bank;
 use Carbon\Carbon;
 use Auth;
 use PDF;
-use App;
 
 
 class memberController extends Controller
@@ -478,9 +477,6 @@ class memberController extends Controller
 	public function pdfMember($id){
       $memberregistration = memberregistration::where('userId',$id)->first();
     //  return view('web.member.pdf.member',compact('memberregistration'));
-      $pdf = App::make('dompdf.wrapper');
-$pdf->loadHTML('<h1>Test</h1>');
-return $pdf->stream();
      $pdf =PDF::loadView('web.member.pdf.member',compact('memberregistration'));
      return $pdf->stream();
 	}
